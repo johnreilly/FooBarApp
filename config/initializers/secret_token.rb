2755e7ed6c1f8a470abdf793f0f40d8cb3cb0f8a
John Reilly <john.reilly@code42.com>
2013-02-28 09:16:28 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FooBarApp::Application.config.secret_token = 'c777819492cb4e8130566e5c3eea99968f2559e2901b211da80989cd33c74bc1d101b8cc684e4f46c97e9c3aeeed04add42385dfe4635e81e4b0c675a5a894f7'
