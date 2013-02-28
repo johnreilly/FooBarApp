c4807b9031bb01e6df4d0ea8a3c84351e9aad6c8
John Reilly <john.reilly@code42.com>
2013-02-28 09:18:19 -0600

    adding model1

---
+++ app/models/model1.rb
@@ -0,0 +1,3 @@
+class Model1 < ActiveRecord::Base
+  # attr_accessible :title, :body
+end
---
+++ db/migrate/20130228151809_create_model1s.rb
@@ -0,0 +1,8 @@
+class CreateModel1s < ActiveRecord::Migration
+  def change
+    create_table :model1s do |t|
+
+      t.timestamps
+    end
+  end
+end
